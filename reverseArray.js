4353e6c531a511849b5e9389842b1adeec033cb7
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-09-01 15:53:16 +0530

    added note to reverse array

--- reverseArray.js
+++ reverseArray.js
@@ -1,6 +1,7 @@
 function reverseArray(arr) {
   let result = [];
   for (let i = arr.length - 1; i >= 0; i--) {
+    //from right to left  it should print i.e 4th index(arr.length -1=>5-1 = 4) to 0th index and we r pushing
     result.push(arr[i]);
   }
   return result;
