9b8b5d66377acd8083af71033d2273b67869f4fc
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-09-01 15:55:54 +0530

    Added note to reverse string

--- reverseString.js
+++ reverseString.js
@@ -1,7 +1,8 @@
 function reverseString(str) {
   let result = "";
   for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i];
+    //from right to left we need to print so that i = str.length -1 => 5-1 =>4
+    result += str[i]; // result = result + srt[i] => 0 => 0l => oll...
   }
   return result;
 }
