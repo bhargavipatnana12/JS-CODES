06de83446b2bd82b3c8324205be954ada540f952
bhargavipatnana <59560435+bhargavipatnana12@users.noreply.github.com>
2026-09-13 22:26:58 +0530

    Create reverseNumber.js

---
+++ reverseNumber.js
@@ -0,0 +1,11 @@
+let n = 123;
+function reverseNumber(n){
+  let reverse = 0;
+  while(n>0){
+    let digit = n%10;
+    reverse = reverse *10 + digit;
+    n = Math.floor(n/10)
+  }
+  return reverse;
+}
+console.log(reverseNumber(n));
